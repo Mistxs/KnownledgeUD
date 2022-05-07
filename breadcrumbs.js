fc68ba1deb444a1fd4b0d9ccdd52a8a11dde941e
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-07 05:42:37 +0300

    Update breadcrumbs.js

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -4,24 +4,17 @@ version 0.01a
 Build by Mistxs for favourite  YCLIENTS team
 */
 
-// let parent = document.querySelector('.container');
-// let before = document.querySelector('.row');
-// let core = document.createElement('div');
-
-// core.id = "breadcrumbs"
-// core.innerHTML = '<ul><li><a href="https://secure.usedesk.ru/settings/support">База знаний</a></li><li class="active">Тест БЗ</li></ul>';
-
-// parent.insertBefore(core, before);
 
 
 let parent = document.querySelector('section .container');
+let core = document.createElement('div');
 
-let p = document.createElement('p');
-p.innerHTML = '!';
+core.id = "breadcrumbs"
+core.innerHTML = '<ul><li><a href="https://secure.usedesk.ru/settings/support">База знаний</a></li><li class="active">Тест БЗ</li></ul>';
 
-parent.prepend(p);
+
+parent.prepend(core);
 
 
 console.log (parent)
-console.log (before)
 console.log (core)
