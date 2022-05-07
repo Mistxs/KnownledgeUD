d980d4860094f8d474cdcef1dc0e5fb35ad81007
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-07 05:20:07 +0300

    Update breadcrumbs.js

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -11,7 +11,7 @@ let core = document.createElement('div');
 core.id = "breadcrumbs"
 core.innerHTML = '<ul><li><a href="https://secure.usedesk.ru/settings/support">База знаний</a></li><li class="active">Тест БЗ</li></ul>';
 
-document.insertBefore(core, before);
+parent.insertBefore(core, before);
 
 console.log (parent)
 console.log (before)
