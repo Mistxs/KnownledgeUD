c22771939c406cd41eefd8155863690ec0b02b14
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-15 01:08:22 +0300

    скрытие уровней крошек

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -11,37 +11,35 @@ let core = document.createElement('div');
 
 core.id = "breadcrumb"
 
-
-parent.prepend(core);
-
-
-// Получение адреса страницы и присвоение вложенности будущих крошек
-var path = document.location.pathname;
-
-var level = 0;
-
-if (path.includes("article")) {
-  level = 3;
-} else if (path.includes("category")) {
-  level = 2;
-  var li_sub = document.querySelector('#breadcrumb .second_li');
-  var li_end = document.querySelector('#breadcrumb .third_li');
-  console.log ("Уровень = " + level);
-  console.log ("Li_sub = " + li_sub);
-  console.log ("Li_end = " + li_end);
-
-} 
-else {level = 1}
-
-
 // Парсинг данных со страницы для крошек
 
 var collection = document.querySelector('#nav-mobile li.active').innerHTML;
 var category = document.querySelector('div.collection a.active');
 var article = document.querySelector('section .row h4').innerHTML;
 
-console.log (collection);
-console.log (category);
-console.log (article);
-
 core.innerHTML = '<ul><li class="first_li">' + collection + '</li> <li class="second_li> <a href="'+ category.href +'">' + category.innerHTML + '</a></li> <li class="third_li">' + article + ' </li></ul>';
+
+parent.prepend(core);
+
+var li_sub = document.querySelector('#breadcrumb .second_li');
+var li_end = document.querySelector('#breadcrumb .third_li');
+
+// Получение адреса страницы и присвоение вложенности будущих крошек
+var path = document.location.pathname;
+var level = 0;
+
+if (path.includes("article")) {
+  level = 3;
+} else if (path.includes("category")) {
+  level = 2;
+  li_end.classList.add("hide");
+} 
+else {
+  level = 1;
+  li_sub.classList.add("hide"); 
+  li_end.classList.add("hide");
+}
+
+  console.log ("Уровень = " + level);
+  console.log ("Li_sub = " + li_sub);
+  console.log ("Li_end = " + li_end);
