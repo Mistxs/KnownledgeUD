082bcb28feaff302ec7a942b358446d3442cf77d
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-15 00:57:06 +0300

    Update breadcrumbs.js

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -26,7 +26,7 @@ if (path.includes("article")) {
   level = 2;
   var li_sub = document.querySelector('#breadcrumb .second_li');
   var li_end = document.querySelector('#breadcrumb .third_li');
-  li_end.classList.add('hide');
+  //li_end.classList.add('hide');
   console.log (level);
    console.log ("Li_sub = " + li_sub);
 
