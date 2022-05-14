7043aabecd55360748af1c6337643dda032b6ec3
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-15 01:36:31 +0300

    Update breadcrumbs.js

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -1,5 +1,5 @@
 /* Breadcrumbs script for Knowledge Base Usedesk 
-version 0.15a
+version 0.16a
 
 Build by Mistxs for favourite  YCLIENTS team
 */
@@ -27,30 +27,17 @@ if (path.includes("article")) {
 } 
 else if (path.includes("category")) {
   level = 2;
-  core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li> <a href="'+ category.href +'">' + category.innerHTML + '</a></li></ul>';
-  // li_end.classList.add("hide");
+  core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li>' + category.innerHTML + '</li></ul>';
 } 
 else if (path.includes("collection")) {
   level = 1;
   core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li>' + collection + '</li></ul>'
-  // li_sub.classList.add("hide"); 
-  // li_end.classList.add("hide");
+
 }
 
 parent.prepend(core);
-
-
-
-
-
-
-// var li_sub = document.querySelector('#breadcrumb .second_li');
-// var li_end = document.querySelector('#breadcrumb .third_li');
-
-
-  console.log ("Уровень = " + level);
-  // console.log ("Li_sub = " + li_sub);
-  // console.log ("Li_end = " + li_end);
+console.log ("Уровень = " + level);
+  
 
 
 
