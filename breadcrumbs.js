197048f45fb4b21681d516bc369c029cb3c9efe4
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-15 01:33:04 +0300

    Update breadcrumbs.js

--- breadcrumbs.js
+++ breadcrumbs.js
@@ -1,5 +1,5 @@
 /* Breadcrumbs script for Knowledge Base Usedesk 
-version 0.01a
+version 0.15a
 
 Build by Mistxs for favourite  YCLIENTS team
 */
@@ -11,36 +11,48 @@ let core = document.createElement('div');
 
 core.id = "breadcrumb"
 
-// Парсинг данных со страницы для крошек
 
-var url = document.location.origin;
+// Парсинг данных со страницы для крошек
 var collection = document.querySelector('#nav-mobile li.active').innerHTML;
 var category = document.querySelector('div.collection a.active');
 var article = document.querySelector('section .row h4').innerHTML;
 
-core.innerHTML = '<ul><li> <a href="' + url + '"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li class="second_li"> <a href="'+ category.href +'">' + category.innerHTML + '</a></li> <li class="third_li">' + article + ' </li></ul>';
-
-parent.prepend(core);
-
-var li_sub = document.querySelector('#breadcrumb .second_li');
-var li_end = document.querySelector('#breadcrumb .third_li');
 
 // Получение адреса страницы и присвоение вложенности будущих крошек
 var path = document.location.pathname;
 var level = 0;
-
 if (path.includes("article")) {
   level = 3;
-} else if (path.includes("category")) {
-  level = 2;
-  li_end.classList.add("hide");
+  core.innerHTML = '<ul><li> <a href="' + url + '"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li class="second_li"> <a href="'+ category.href +'">' + category.innerHTML + '</a></li> <li class="third_li">' + article + ' </li></ul>';
 } 
-else {
+else if (path.includes("category")) {
+  level = 2;
+  core.innerHTML = '<ul><li> <a href="' + url + '"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li> <a href="'+ category.href +'">' + category.innerHTML + '</a></li></ul>';
+  // li_end.classList.add("hide");
+} 
+else if (path.includes("collection")) {
   level = 1;
-  li_sub.classList.add("hide"); 
-  li_end.classList.add("hide");
+  core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li>' + collection + '</li></ul>'
+  // li_sub.classList.add("hide"); 
+  // li_end.classList.add("hide");
 }
 
+parent.prepend(core);
+
+
+
+
+
+
+// var li_sub = document.querySelector('#breadcrumb .second_li');
+// var li_end = document.querySelector('#breadcrumb .third_li');
+
+
   console.log ("Уровень = " + level);
-  console.log ("Li_sub = " + li_sub);
-  console.log ("Li_end = " + li_end);
+  // console.log ("Li_sub = " + li_sub);
+  // console.log ("Li_end = " + li_end);
+
+
+
+
+
