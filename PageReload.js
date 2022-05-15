3b434e2cf9f0249c753e90e8dd144228f91c7bea
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-15 03:00:08 +0300

    Update PageReload.js

--- PageReload.js
+++ PageReload.js
@@ -9,14 +9,21 @@ var path = document.location.pathname;
 var level = 0;
 if (path.includes("article")) {
   level = 3;
-  core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li class="second_li"> <a href="'+ category.href +'">' + category.innerHTML + '</a></li> <li class="third_li">' + article + ' </li></ul>';
+  // core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li class="second_li"> <a href="'+ category.href +'">' + category.innerHTML + '</a></li> <li class="third_li">' + article + ' </li></ul>';
 } 
 else if (path.includes("category")) {
   level = 2;
-  core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li>' + category.innerHTML + '</li></ul>';
+  // core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li class="first_li">' + collection + '</li> <li>' + category.innerHTML + '</li></ul>';
 } 
 else if (path.includes("collection")) {
   level = 1;
-  core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li>' + collection + '</li></ul>'
-
+  // core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li>' + collection + '</li></ul>'
 }
+
+
+
+
+
+// Отладка
+
+console.log ("Уровень страницы - "+level);
