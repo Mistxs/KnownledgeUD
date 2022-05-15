f534eb6b8becf6493be717d62734644239a9a987
Mistxs <96961160+Mistxs@users.noreply.github.com>
2022-05-15 03:47:32 +0300

    Update PageReload.js

--- PageReload.js
+++ PageReload.js
@@ -19,13 +19,10 @@ else if (path.includes("collection")) {
   level = 1;
   var listofpages = document.querySelector('.container .collection'); // левый список со страницами
   var pagecont = document.querySelector('.container .row').lastElementChild;
-  const buffer = pagecont; // записываем в буфер все что внутри pagecont
-
-
-
-  document.querySelector('.container .row').lastElementChild.innerHTML = listofpages.innerHTML; // меняем содержимое страницы на список статей
-  document.querySelector('.container .collection').innerHTML = "херабора"; // переписыаем список страниц на популярные статьи
-  // delete buffer; // удаляем буфер из памяти
+  var clone = pagecont.cloneNode(true);  // Создадим копию содержимого из основного блока страницы в память в переменную clone
+  pagecont.innerHTML = listofpages.innerHTML;
+  listofpages.innerHTML = clone.innerHTML;
+  delete clone; // удаляем буфер из памяти
 
   
   // core.innerHTML = '<ul><li> <a href="/"> Главная </a> </li> <li>' + collection + '</li></ul>'
@@ -40,4 +37,4 @@ else if (path.includes("collection")) {
 console.log ("Уровень страницы - "+level);
 console.log (listofpages.innerHTML);
 console.log (pagecont.innerHTML);
-console.log (buffer.innerHTML);
+console.log (clone.innerHTML);
